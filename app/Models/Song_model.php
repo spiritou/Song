0a5bce6d4fa866279023e16fb7dc04e7ee91710b
Mbanzoulou <o2mhova@gmail.com>
2025-10-06 23:03:15 +0100

    creating the song model

---
+++ app/Models/Song_model.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Song_model;  
+use App\Core\Database;
+
+class Song {
+    private $id;
+    private $name;
+    private $conn;
+
+    public function __construct(Database $conn)
+    {
+        $this->conn = $conn->getConnection();
+    }
+
+    public function save($name) 
+    {
+        $stmt = $this->conn->prepare("INSERT INTO songs (name) VALUES (:name)");
+        $stmt->bindParam(':name', $name);
+        return $stmt->execute();
+    }
+
+
+    
+}
