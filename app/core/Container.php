b4a0bcd7cbcf914415e128bb6c3df5de5c4ca82b
Mbanzoulou <o2mhova@gmail.com>
2025-10-10 20:52:41 +0100

    creating a container

---
+++ app/core/Container.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core;
+
+class container 
+{
+    public function get($class)
+    {
+        $container = new \ReflectionClass($class);
+        $constructor = $container->getConstructor();
+        if(is_null($constructor))
+        {
+            return new $class;
+        }
+
+        $dependencies = [];
+        foreach($constructor->getParameters() as $parameter)
+        {
+            $type = $parameter->gettype();
+            if($type && !$type->isBuiltin())
+            {
+               $deepclass = $type->getName();
+                $dependencies[] = $this->get($deepclass);
+                // var_dump($dependencies);
+            }
+           
+        }
+    }
+}
