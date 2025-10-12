ca21762969b5e47e819fe96dd7a8f165a645bd16
Mbanzoulou <o2mhova@gmail.com>
2025-10-12 21:40:57 +0100

    problem running the add button while on other devices trying to CORS acess control

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
+// Allow CORS for API requests
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Handle preflight OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 require_once __DIR__ . '/../vendor/autoload.php';
