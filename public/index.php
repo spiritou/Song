7a8802a3b1676156377a8924564b5596412c5e6a
Mbanzoulou <o2mhova@gmail.com>
2025-10-19 15:45:59 +0100

    making the update methode take the parameter into account

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ $router->get('/', 'Songcontroller@index');
 $router->post('/api/songs', 'Songcontroller@store');
 $router->get('/api/songs', 'Songcontroller@getAllsongs');
 $router->delete('/api/songs/{id}', 'Songcontroller@delete');
-$router->put('/api/songs', 'Songcontroller@update');
+$router->put('/api/songs/{id}', 'Songcontroller@update');
 $router->get('/api/songs/{id}', 'Songcontroller@show');
 // list($controller, $function) = $router->run();
 list($callback, $params) = $router->run();
