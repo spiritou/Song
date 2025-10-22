82e5e7187a13c56da53e61278dc676956d9cf2fa
Mbanzoulou <o2mhova@gmail.com>
2025-10-22 11:08:33 +0100

    change my select all query so the result is based on last_update

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -22,7 +22,7 @@ class Song {
 
     public function getAll() 
     {
-        $stmt = $this->conn->prepare("SELECT * FROM songs ORDER BY id DESC");
+        $stmt = $this->conn->prepare("SELECT * FROM songs ORDER BY last_update DESC");
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }   
