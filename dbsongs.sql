8533110743aa5e33a9c3306787e56c4893ca3b3a
Mbanzoulou <o2mhova@gmail.com>
2025-11-16 16:18:05 +0100

    added a users table to my database

--- dbsongs.sql
+++ dbsongs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 10, 2025 at 08:11 PM
+-- Generation Time: Nov 16, 2025 at 04:14 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,17 +31,35 @@ CREATE TABLE `songs` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_songs` tinyint(1) DEFAULT 0
+  `deleted_songs` tinyint(1) DEFAULT 0,
+  `users_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `songs`
 --
 
-INSERT INTO `songs` (`id`, `name`, `last_update`, `deleted_songs`) VALUES
-(72, 'We are one', '2025-11-06 21:48:46', 0),
-(74, 'never say never', '2025-11-06 21:48:23', 0),
-(76, 'moves', '2025-11-06 21:54:22', 0);
+INSERT INTO `songs` (`id`, `name`, `last_update`, `deleted_songs`, `users_id`) VALUES
+(72, 'We are one', '2025-11-11 22:45:49', 1, NULL),
+(74, 'never say never', '2025-11-13 19:21:57', 1, NULL),
+(76, 'moves', '2025-11-11 22:42:48', 1, NULL),
+(77, 'Yess', '2025-11-13 04:42:38', 1, NULL),
+(78, 'only God can judge me', '2025-11-15 14:52:44', 0, NULL),
+(79, 'sexy red', '2025-11-15 14:52:14', 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` enum('user','admin') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -51,6 +69,13 @@ INSERT INTO `songs` (`id`, `name`, `last_update`, `deleted_songs`) VALUES
 -- Indexes for table `songs`
 --
 ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -61,7 +86,23 @@ ALTER TABLE `songs`
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
