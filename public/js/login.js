7e16c360a74ba9b7802b34017ff3e4d8a9947da6
Mbanzoulou <o2mhova@gmail.com>
2025-11-20 23:02:10 +0100

    set up the login javascript

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,25 @@
 // the javascript code for login page
+
+document.getElementById('loginForm').addEventListner('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('name').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (!username || !password) {
+        return alert('Please enter both username and password');
+    }
+
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ username, password})
+        });
+
+        const data = await response.json();
+        if (data.success) {
+            window.location.href = '/dashboard'; // redirect to dashboard or home page
+        } else {
+            alert(data.error || 'Login failed');
+        }
+
+    });
