28cb945f76e2b73b7ef7d0e1587d51428a72d553
Mbanzoulou <o2mhova@gmail.com>
2025-11-27 00:18:35 +0100

    added header in the logout function and cleared the cookie set by session

--- app/Controllers/Authenticationcontroller.php
+++ app/Controllers/Authenticationcontroller.php
@@ -43,11 +43,23 @@ class Authenticationcontroller
 
     public function logout()
     {
+        header('Content-Type: application/json');
+        
         if(session_status() == PHP_SESSION_NONE){
             session_start();
         }
+        
         session_unset();
         session_destroy();
+        
+        // Clear the session cookie
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
 
         echo json_encode(['success' => true]);
     }
