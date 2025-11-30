a3f725c52011c3f120777e35118e1b6dfacbfc0d
Mbanzoulou <o2mhova@gmail.com>
2025-11-30 17:43:03 +0100

    now moving up to update and delete

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -24,7 +24,7 @@ class Song {
 
     public function getAllSongsbyID($user_id) 
     {
-        $stmt = $this->conn->prepare("SELECT * FROM songs WHERE deleted_songs = 0 AND users_id :user_id ORDER BY last_update DESC");
+        $stmt = $this->conn->prepare("SELECT * FROM songs WHERE deleted_songs = 0 AND users_id = :user_id ORDER BY last_update DESC");
         $stmt->bindParam(':user_id', $user_id);
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
